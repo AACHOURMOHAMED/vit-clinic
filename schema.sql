bd8e62f409f1e5b75dbde21d5cf4d1286b68a2ad
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-09-17 11:48:18 +0100

    Add queries

--- schema.sql
+++ schema.sql
@@ -9,3 +9,29 @@ CREATE TABLE animals (
     weight_kg DECIMAL NOT NULL,
     species VARCHAR(50)  NULL
 );
+
+
+CREATE TABLE owners (
+owners_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+full_name varchar(100) ,
+age INT
+);
+
+
+
+CREATE TABLE species (
+species_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+name varchar(100)
+);
+
+
+-- drop column
+ALTER TABLE animals DROP COLUMN species;
+
+-- add forign key
+ALTER TABLE animals
+ADD species_id INT REFERENCES species(species_id);
+
+
+ALTER TABLE animals
+ADD owners_id INT REFERENCES owners(owners_id);
